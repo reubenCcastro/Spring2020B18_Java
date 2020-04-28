513e771e594e8c1c7feaf0a0aa91f2790cb842e1
Reuben Castro <castro.reuben@gmail.com>
2020-04-28 08:49:26 -0600

    day30 Class

---
+++ IdeaProjects/Spring2020B18_Java/src/day30_ArrayListCont/WarmUpClass.java
@@ -0,0 +1,22 @@
+package day30_ArrayListCont;
+/*
+WARMUP TASKS:
+
+
+	3. write a program that can print out the unique elements from an int array
+		   HINT: store all the unqie elemenbts of array into a list of integers
+		   Ex: array==> {1,1,2,3,3,4,5}
+		   	   output: {2,4,5}
+    4.	write a program that can find the unique elements from an ArrayList of integers and stores into another list called uniques
+    		ex: list==> {1,1,2,3,3,4,5}
+    			uniques ==> {2,4,5}
+    5. write a program that can combine two String arrays into one arrayList
+    		ex:
+    			arr1 = {"A", "B", "C"};
+    			arr2 = {"D", "E", "F", "G"};
+    			list ==> {"A", "B", "C", "D", "E", "F", "G"}
+ */
+public class WarmUpClass {
+
+
+}
